e96081afc5b4f18e332b7994e40c6fc8708ecd4d
jyotipriya  jena <163876310+jyotipriya1@users.noreply.github.com>
2024-06-22 12:14:07 +0000

    condition

--- 01-basic-js/data types.js
+++ 01-basic-js/data types.js
@@ -1,30 +1,43 @@
-//let age=23;
-//console.log(age)
-//console.log(typeof(age))
+// let age = 23;
+// console.log(age)
+// console.log(typeof(age))
 
 // var a = "web bocket"
 // console.log(a)
 // console.log(typeof(a))
 
-//const isPayment = true;
-//console.log(isPayment)
-//console.log(typeof(isPayment))
+// const isPayment = true;
+// console.log(isPayment)
+// console.log(typeof(isPayment))
 
-let x = Bigint(123);
-console.log(x)
-console.log(typeof(x))
+// let x = BigInt("890");
+// console.log(x)
+// console.log(typeof(x))
 
-//let y = symbol("hello!");
-//console.log(y)
-//console.log(y.description) //it show the unique value of symbol
-//console.log(typeof(y))
-//symbol gives us a unique property so that we used symbol in javascript.
+let y = Symbol("hello!");
+console.log(y)
+console.log(y.description) // its show the unique value of symbol
+console.log(typeof(y)) 
+// symbol gives us a unique property so that we used symbol in javascript.
 
-//const a = null;
-//console.log(a) //null
-//console.log(typeof(a)) //object
+// const a = null;
+// console.log(a) // null
+// console.log(typeof(a)) // object
 
-//let mysalary;
-//console.log(mysalary)
-//console.log(typeof(mysalary))
+// let mySalary;
+// console.log(mySalary)
+// console.log(typeof(mySalary))
 
+jyoti = {
+    college : "GIET",
+    address : "BBSR",
+    age : 22,
+    salary :50000,
+    carrier : "Good"
+}
+
+console.log(jyoti)
+console.log(jyoti["college"])
+console.log(jyoti["address"])
+console.log(jyoti["age"])
+console.log(jyoti["salary"])
